6edd1b05d23bbdb29f9efa1804adccc82731c8d4
Alexis Langlet <alexis.langlet@etu.umontpellier.fr>
2022-05-25 17:44:59 +0200

    get rid of fruits

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,3 @@
-INSERT INTO known_fruits(id, name) VALUES (1, 'Cherry');
-INSERT INTO known_fruits(id, name) VALUES (2, 'Apple');
-INSERT INTO known_fruits(id, name) VALUES (3, 'Banana');
-
 INSERT INTO known_city(id, name, department_code, insee_code, zip_code, lat, lon) VALUES (1,'Montpellier', '34000', '34090', '34', 43.6, 3.88);
 INSERT INTO known_city(id, name, department_code, insee_code, zip_code, lat, lon) VALUES (2,'Paris', '75000', '75000', '75', 43.8, 3.98);
 INSERT INTO known_city(id, name, department_code, insee_code, zip_code, lat, lon) VALUES (3,'Avignon', '84000', '84000', '84', 48.8, 2.27);
