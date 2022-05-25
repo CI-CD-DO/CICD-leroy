c4e4fb5b784c6f8db92ec6d4384acf02e6aae81b
Alexis Langlet <alexis.langlet@etu.umontpellier.fr>
2022-05-25 15:22:25 +0200

    healthcheck

--- src/main/java/org/acme/hibernate/orm/HealthResource.java
+++ src/main/java/org/acme/hibernate/orm/HealthResource.java
@@ -18,7 +18,6 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
-import org.jboss.logging.Logger;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -29,10 +28,40 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 @Consumes("application/json")
 public class HealthResource {
 
-    private static final Logger LOGGER = Logger.getLogger(CityResource.class.getName());
+    @Inject
+    EntityManager entityManager;
 
     @GET
     public Response get() {
         return Response.ok().status(204).build();
+    }
+
+    @Provider
+    public static class ErrorMapper implements ExceptionMapper<Exception> {
+
+        @Inject
+        ObjectMapper objectMapper;
+
+        @Override
+        public Response toResponse(Exception exception) {
+
+            int code = 500;
+            if (exception instanceof WebApplicationException) {
+                code = ((WebApplicationException) exception).getResponse().getStatus();
+            }
+
+            ObjectNode exceptionJson = objectMapper.createObjectNode();
+            exceptionJson.put("exceptionType", exception.getClass().getName());
+            exceptionJson.put("code", code);
+
+            if (exception.getMessage() != null) {
+                exceptionJson.put("error", exception.getMessage());
+            }
+
+            return Response.status(code)
+                    .entity(exceptionJson)
+                    .build();
+        }
+
     }
 }
