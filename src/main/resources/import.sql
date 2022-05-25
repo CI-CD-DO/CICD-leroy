fe9bb6432531c2e95eb8a1150704d5dda96d04be
GridexX <arsene582@gmail.com>
2022-05-25 15:31:05 +0200

    fix: add docker workflow, imports and delete fruits resource

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,6 +2,6 @@ INSERT INTO known_fruits(id, name) VALUES (1, 'Cherry');
 INSERT INTO known_fruits(id, name) VALUES (2, 'Apple');
 INSERT INTO known_fruits(id, name) VALUES (3, 'Banana');
 
-INSERT INTO known_city(id, name, department_code, insee_code, zip_code, lat, lon) VALUES (1,'Montpellier', '34000', '34090', '34090', 43.6, 3.88);
--- INSERT INTO known_city(default,"84000", "34", "34090", "Avignon", 43.8, 3.98);
--- INSERT INTO known_city(default,"75000", "75", "75090", "Paris", 48.8, 2.27);
+INSERT INTO known_city(id, name, department_code, insee_code, zip_code, lat, lon) VALUES (1,'Montpellier', '34000', '34090', '34', 43.6, 3.88);
+INSERT INTO known_city(id, name, department_code, insee_code, zip_code, lat, lon) VALUES (2,'Paris', '75000', '75000', '75', 43.8, 3.98);
+INSERT INTO known_city(id, name, department_code, insee_code, zip_code, lat, lon) VALUES (3,'Avignon', '84000', '84000', '84', 48.8, 2.27);
